a3fce741d4dd351a6a960f72ee6ab2216ad168d7
Edison Chan <edisonchanwk@gmail.com>
2024-05-12 20:47:40 +0800

    Update benchmark use ollama

--- benchmark/benchmark.py
+++ benchmark/benchmark.py
@@ -1,8 +1,31 @@
 import typer
+import ollama
 
 
-def main(models):
-    pass
+def stream_model_response(model: str, message: str, handler: callable):
+    stream = ollama.chat(model="codellama", message="Hello, how are you?", stream=True)
+
+    for chunk in stream:
+        message = chunk["message"]["content"]
+        handler(message)
+        # print(chunk["message"]["content"], end="", flush=True)
+
+
+def get_model_response(model: str, message: str):
+    response = ollama.chat(model=model, messages=[{"role": "user", "content": message}])
+    return response["message"]["content"]
+
+
+def main(model: str, message: str):
+    try:
+        ollama.show(model)
+    except ollama.ResponseError:
+        print(f"Model {model} not found. Pulling...")
+        ollama.pull(model)
+
+    # def main(models: list[str], tests_path: str):
+    # `test` file will be a codeql csv response with a `label` column (1 - filtered, 0 - not filtered)
+    print(get_model_response(model, message))
 
 
 def run_tests_on_model(model: str, tests_path: str):
